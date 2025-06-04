88d96812f7cce87e16b1e0bf4c47d3957a5d707d
Petter Reingardt <89352653+Frogblast@users.noreply.github.com>
2025-06-04 22:30:00 +0200

    creepy lighting

--- Assets/Scripts/General/FollowTarget.cs
+++ Assets/Scripts/General/FollowTarget.cs
@@ -4,14 +4,22 @@ public class FollowTarget : MonoBehaviour
 {
     [SerializeField] private Transform target;
     private Vector3 offset;
+    private Vector3 rotationOffset;
+
+    [SerializeField] private bool enableRotation;
 
     private void Awake()
     {
         offset = transform.position;
+        rotationOffset = transform.rotation.eulerAngles;
     }
 
     private void Update()
     {
         transform.position = target.position + offset;
+        if (enableRotation)
+        {
+            transform.rotation = Quaternion.Euler(target.rotation.eulerAngles + rotationOffset);
+        }
     }
 }
